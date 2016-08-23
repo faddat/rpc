adff6b105f49509187e7f40c7e1731a9d78cdb89
Ondřej Kupka <ondra.cap@gmail.com>
2016-08-23 16:19:07 +0200

    wip [ci skip]

--- apis/types/int.go
+++ apis/types/int.go
@@ -1,30 +1,91 @@
 package types
 
 import (
+	// Stdlib
 	"encoding/json"
-	"math/big"
+	"strconv"
+
+	// Vendor
+	"github.com/pkg/errors"
 )
 
-type Int struct {
-	*big.Int
+func unmarshalInt(data []byte) (int64, error) {
+	if len(data) == 0 {
+		return 0, errors.New("types: empty data received when unmarshalling an integer")
+	}
+
+	var (
+		i   int64
+		err error
+	)
+	if data[0] == '"' {
+		d := data[1:]
+		d = d[:len(d)-1]
+		i, err = strconv.ParseInt(string(d), 10, 64)
+	} else {
+		err = json.Unmarshal(data, &i)
+	}
+	return i, errors.Wrapf(err, "types: failed to unmarshal int: %v", data)
 }
 
-func (num *Int) UnmarshalJSON(data []byte) error {
-	if data[0] == '"' {
-		data = data[1:]
-		data = data[:len(data)-1]
-		var value big.Int
-		if err := json.Unmarshal(data, &value); err != nil {
-			return err
-		}
-		num.Int = &value
-		return nil
-	}
+/*
+type Int int
 
-	var value int64
-	if err := json.Unmarshal(data, &value); err != nil {
+func (num *Int) UnmarshalJSON(data []byte) error {
+	v, err := unmarshalInt(data)
+	if err != nil {
 		return err
 	}
-	num.Int = big.NewInt(value)
+
+	*num = Int(v)
+	return nil
+}
+*/
+
+type Int8 int8
+
+func (num *Int8) UnmarshalJSON(data []byte) error {
+	v, err := unmarshalInt(data)
+	if err != nil {
+		return err
+	}
+
+	*num = Int8(v)
+	return nil
+}
+
+type Int16 int16
+
+func (num *Int16) UnmarshalJSON(data []byte) error {
+	v, err := unmarshalInt(data)
+	if err != nil {
+		return err
+	}
+
+	*num = Int16(v)
+	return nil
+}
+
+type Int32 int32
+
+func (num *Int32) UnmarshalJSON(data []byte) error {
+	v, err := unmarshalInt(data)
+	if err != nil {
+		return err
+	}
+
+	*num = Int32(v)
+	return nil
+}
+
+type Int64 int64
+
+func (num *Int64) UnmarshalJSON(data []byte) error {
+	v, err := unmarshalInt(data)
+	if err != nil {
+		return err
+	}
+
+	*num = Int64(v)
 	return nil
 }
