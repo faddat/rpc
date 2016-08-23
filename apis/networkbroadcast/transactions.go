b23fe34bc62aabb84ead0e3d7e56d87fee5d128c
Ondřej Kupka <ondra.cap@gmail.com>
2016-08-23 14:14:01 +0200

    wip [ci skip]

--- apis/networkbroadcast/transactions.go
+++ apis/networkbroadcast/transactions.go
@@ -6,6 +6,9 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 
+	// RPC
+	"github.com/go-steem/rpc/apis/types"
+
 	// Vendor
 	"github.com/pkg/errors"
 )
@@ -17,8 +20,42 @@ type Transaction struct {
 	Expiration     *types.Time
 }
 
-func Serialize() (*SignedTransaction, error) {
+func (t *Transaction) Serialize() ([]byte, error) {
+	// Write buffer.
+	var b bytes.Buffer
 
+	// Get ready for varints.
+	varintBuffer := make([]byte, binary.MaxVarintLen64)
+
+	putVarint := func(x int64) error {
+		n := binary.PutVarint(varintBuffer, x)
+		data := varintBuffer[:n]
+		_, err := b.Write(data)
+		return err
+	}
+
+	// Write ref_block_num.
+	if err := binary.Write(&b, binary.LittleEndian, t.RefBlockNum); err != nil {
+		return nil, errors.Wrapf(err, "networkbroadcast: failed to encode RefBlockNum: %v", t.RefBlockNum)
+	}
+
+	// Write ref_block_prefix.
+	if err := binary.Write(&b, binary.LittleEndian, t.RefBlockPrefix); err != nil {
+		return nil, errors.Wrapf(err, "networkbroadcast: failed to encode RefBlockPrefix: %v", t.RefBlockPrefix)
+	}
+
+	// Write expiration.
+	timestamp := uint32(t.Expiration.Unix())
+	if err := binary.Write(&b, binary.LittleEndian, timestamp); err != nil {
+		return nil, errors.Wrapf(err, "networkbroadcast: failed to encode Expiration: %v (Unix)", timestamp)
+	}
+
+	// Write the number of operations.
+	if err := putVarint(int64(len(t.Operations))); err != nil {
+		return nil, errors.Wrap(err, "networkbroadcast: failed to encode Operations length")
+	}
+
+	return b.Bytes(), nil
 }
 
 func RefBlockNum(blockNumber uint32) uint16 {
