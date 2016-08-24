1a60d4d7579d9372b20ee544d4ec1517ebbe6120
Ondřej Kupka <ondra.cap@gmail.com>
2016-08-24 02:50:05 +0200

    wip [ci skip]

--- apis/networkbroadcast/sign.c
+++ apis/networkbroadcast/sign.c
@@ -1,1 +1,8 @@
 #include "sign.h"
+#include "secp256k1.h"
+#include "secp256k1_recovery.h"
+
+// https://github.com/bitcoin/bitcoin/blob/a6a860796a44a2805a58391a009ba22752f64e32/src/secp256k1/include/secp256k1_recovery.h#L81
+void sign() {
+	secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
+}
