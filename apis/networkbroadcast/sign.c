c223dd0b4e647310cf50b66fd2f9e979ddf24103
Ondřej Kupka <ondra.cap@gmail.com>
2016-08-24 03:41:02 +0200

    wip [ci skip]

--- apis/networkbroadcast/sign.c
+++ apis/networkbroadcast/sign.c
@@ -1,8 +1,33 @@
 #include "sign.h"
 #include "secp256k1.h"
 #include "secp256k1_recovery.h"
+#include "ecdsa.h"
 
-// https://github.com/bitcoin/bitcoin/blob/a6a860796a44a2805a58391a009ba22752f64e32/src/secp256k1/include/secp256k1_recovery.h#L81
-void sign() {
+int sign()
+
+int sign(
+	const unsigned char *digest,
+	const unsigned char *privkey,
+	const void *ndata,
+	char *signature,
+	int *recid
+) {
+	// Prepare a context.
 	secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
+
+	// Prepare a signature.
+	secp256k1_ecdsa_recoverable_signature sig;
+
+	// Sign the digest using the given private key.
+	if (!secp256k1_ecdsa_sign_recoverable(ctx, &sig, msg32, privkey, NULL, ndata)) {
+		secp256k1_context_destroy(ctx);
+		return 0;
+	}
+
+	// Serialize.
+	secp256k1_ecdsa_recoverable_signature_serialize_compact(ctx, signature, &recid, &sig);
+
+	// Destroy the context and return succcess.
+	secp256k1_context_destroy(ctx);
+	return 1;
 }
