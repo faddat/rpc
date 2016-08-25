7cde2b2e8860af304ae7f450f8a10254d2372572
Ondřej Kupka <ondra.cap@gmail.com>
2016-08-25 14:11:18 +0200

    wip [ci skip]

--- apis/networkbroadcast/transactions_signing_test.go
+++ apis/networkbroadcast/transactions_signing_test.go
@@ -4,11 +4,46 @@ import (
 	// Stdlib
 	"encoding/hex"
 	"testing"
+	"time"
+
+	// RPC
+	"github.com/go-steem/rpc/apis/database"
+	"github.com/go-steem/rpc/encoding/wif"
+	"github.com/go-steem/rpc/types"
 )
 
 func TestTransaction_Sign(t *testing.T) {
+	var (
+		WIF            = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
+		refBlockNum    = 34294
+		refBlockPrefix = 3707022213
+		expiration     = time.Date(2016, 4, 6, 8, 29, 27, 0, time.UTC)
+	)
+
+	tx := database.Transaction{
+		RefBlockNum:    types.UInt16(refBlockNum),
+		RefBlockPrefix: types.UInt32(refBlockPrefix),
+		Expiration:     &types.Time{&expiration},
+		Operations: []*database.Operation{
+			{
+				database.OpTypeVote,
+				&database.VoteOperation{
+					Voter:    "foobara",
+					Author:   "foobarc",
+					Permlink: "foobard",
+					Weight:   1000,
+				},
+			},
+		},
+	}
+
+	privKey, err := wif.Decode(WIF)
+	if err != nil {
+		t.Error(err)
+	}
+
 	privKeys := [][]byte{
-		[]byte("5JLw5dgQAx6rhZEgNN5C2ds1V47RweGshynFSWFbaMohsYsBvE8"),
+		[]byte(privKey),
 	}
 
 	sigs, err := Sign(&tx, SteemChain, privKeys)
