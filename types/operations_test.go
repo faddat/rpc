08ba43310dc3c125560a682fe7ba80d98776134e
Jacob Gadikian <faddat@gmail.com>
2016-09-21 07:03:11 +0700

    Update operations_test.go

--- types/operations_test.go
+++ types/operations_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	// RPC
-	"github.com/go-steem/rpc/encoding/transaction"
+	"github.com/faddat/rpc/encoding/transaction"
 )
 
 func TestVoteOperation_MarshalTransaction(t *testing.T) {
